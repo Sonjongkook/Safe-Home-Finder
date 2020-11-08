acacfd18d2017a022101ef6bc976adb5d00a51bb
Sonjongkook <63791870+Sonjongkook@users.noreply.github.com>
2020-11-08 04:15:54 -0600

    Great schools api implement
    Getting schools list based on Great schools api

--- Source/SafeHomeFinder/src/app/app.module.ts
+++ Source/SafeHomeFinder/src/app/app.module.ts
@@ -2,14 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    HttpClientModule
+],
   providers: [],
   bootstrap: [AppComponent]
 })
