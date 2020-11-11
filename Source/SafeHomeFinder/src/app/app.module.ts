8acedb888f284005fbe273f58171fcddb3e05262
Sonjongkook <63791870+Sonjongkook@users.noreply.github.com>
2020-11-11 10:26:43 -0600

    Adding component and route
    I added a result componenet which shows the result of the search. Also create routing-module.ts which handles routing and navigating

--- Source/SafeHomeFinder/src/app/app.module.ts
+++ Source/SafeHomeFinder/src/app/app.module.ts
@@ -5,15 +5,19 @@ import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
 import { AgmCoreModule } from '@agm/core';
+import { ResultComponent } from './result/result.component';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    ResultComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    AppRoutingModule,
     //change the api key for your local area
     AgmCoreModule.forRoot({apiKey: 'AIzaSyCKAgIKofveuwkVAzggpI55qLtGqBidlDk'})
 ],
